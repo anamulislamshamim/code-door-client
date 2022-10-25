ddf2081da8affbc08c63008613b781a7afaea03d
anamulislamshamim <anamulislamshamim117@gmail.com>
2022-10-25 08:44:22 +0600

    added a coding animation and also some welcoming text in the home page

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -4,18 +4,18 @@ export const Nav = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
   
     return (
-      <div class="bg-gray-900">
-        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-          <div class="relative flex items-center justify-between">
-            <div class="flex items-center">
+      <div className="bg-gray-900">
+        <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div className="relative flex items-center justify-between">
+            <div className="flex items-center">
               <a
                 href="/"
                 aria-label="Company"
                 title="Company"
-                class="inline-flex items-center mr-8"
+                className="inline-flex items-center mr-8"
               >
                 <svg
-                  class="w-8 text-teal-accent-400"
+                  className="w-8 text-teal-accent-400"
                   viewBox="0 0 24 24"
                   strokeLinejoin="round"
                   strokeWidth="2"
@@ -29,17 +29,17 @@ export const Nav = () => {
                   <rect x="14" y="1" width="7" height="6" />
                   <rect x="14" y="11" width="7" height="12" />
                 </svg>
-                <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
                   Company
                 </span>
               </a>
-              <ul class="flex items-center hidden space-x-8 lg:flex">
+              <ul className="flex items-center hidden space-x-8 lg:flex">
                 <li>
                   <a
                     href="/"
                     aria-label="Our product"
                     title="Our product"
-                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                   >
                     Product
                   </a>
@@ -49,7 +49,7 @@ export const Nav = () => {
                     href="/"
                     aria-label="Our product"
                     title="Our product"
-                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                   >
                     Features
                   </a>
@@ -59,7 +59,7 @@ export const Nav = () => {
                     href="/"
                     aria-label="Product pricing"
                     title="Product pricing"
-                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                   >
                     Pricing
                   </a>
@@ -69,20 +69,20 @@ export const Nav = () => {
                     href="/"
                     aria-label="About us"
                     title="About us"
-                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                   >
                     About us
                   </a>
                 </li>
               </ul>
             </div>
-            <ul class="flex items-center hidden space-x-8 lg:flex">
+            <ul className="flex items-center hidden space-x-8 lg:flex">
               <li>
                 <a
                   href="/"
                   aria-label="Sign in"
                   title="Sign in"
-                  class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                  className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                 >
                   Sign in
                 </a>
@@ -90,7 +90,7 @@ export const Nav = () => {
               <li>
                 <a
                   href="/"
-                  class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                   aria-label="Sign up"
                   title="Sign up"
                 >
@@ -98,14 +98,14 @@ export const Nav = () => {
                 </a>
               </li>
             </ul>
-            <div class="lg:hidden">
+            <div className="lg:hidden">
               <button
                 aria-label="Open Menu"
                 title="Open Menu"
-                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
                 onClick={() => setIsMenuOpen(true)}
               >
-                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
                     d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -121,18 +121,18 @@ export const Nav = () => {
                 </svg>
               </button>
               {isMenuOpen && (
-                <div class="absolute top-0 left-0 w-full">
-                  <div class="p-5 bg-white border rounded shadow-sm">
-                    <div class="flex items-center justify-between mb-4">
+                <div className="absolute top-0 left-0 w-full">
+                  <div className="p-5 bg-white border rounded shadow-sm">
+                    <div className="flex items-center justify-between mb-4">
                       <div>
                         <a
                           href="/"
                           aria-label="Company"
                           title="Company"
-                          class="inline-flex items-center"
+                          className="inline-flex items-center"
                         >
                           <svg
-                            class="w-8 text-deep-purple-accent-400"
+                            className="w-8 text-deep-purple-accent-400"
                             viewBox="0 0 24 24"
                             strokeLinejoin="round"
                             strokeWidth="2"
@@ -146,7 +146,7 @@ export const Nav = () => {
                             <rect x="14" y="1" width="7" height="6" />
                             <rect x="14" y="11" width="7" height="12" />
                           </svg>
-                          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                             Company
                           </span>
                         </a>
@@ -155,10 +155,10 @@ export const Nav = () => {
                         <button
                           aria-label="Close Menu"
                           title="Close Menu"
-                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -168,13 +168,13 @@ export const Nav = () => {
                       </div>
                     </div>
                     <nav>
-                      <ul class="space-y-4">
+                      <ul className="space-y-4">
                         <li>
                           <a
                             href="/"
                             aria-label="Our product"
                             title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             Product
                           </a>
@@ -184,7 +184,7 @@ export const Nav = () => {
                             href="/"
                             aria-label="Our product"
                             title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             Features
                           </a>
@@ -194,7 +194,7 @@ export const Nav = () => {
                             href="/"
                             aria-label="Product pricing"
                             title="Product pricing"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             Pricing
                           </a>
@@ -204,7 +204,7 @@ export const Nav = () => {
                             href="/"
                             aria-label="About us"
                             title="About us"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             About us
                           </a>
@@ -214,7 +214,7 @@ export const Nav = () => {
                             href="/"
                             aria-label="Sign in"
                             title="Sign in"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             Sign in
                           </a>
@@ -222,7 +222,7 @@ export const Nav = () => {
                         <li>
                           <a
                             href="/"
-                            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                             aria-label="Sign up"
                             title="Sign up"
                           >
